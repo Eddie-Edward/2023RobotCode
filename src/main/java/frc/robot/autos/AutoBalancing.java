68891f41e7805087998923347567e0c743ca5a82
Jason Deal <jasond@dde-inc.com>
2023-03-30 18:46:14 -0400

    fix: auto-balance on timer (#21)

--- src/main/java/frc/robot/autos/AutoBalancing.java
+++ src/main/java/frc/robot/autos/AutoBalancing.java
@@ -16,65 +16,75 @@ import frc.robot.drivetrain.Drivetrain;
 import frc.robot.drivetrainOld.SwerveDrivetrain;
 
 public class AutoBalancing extends CommandBase {
-  /** Creates a new AutoBalancing. */
-  private final Drivetrain mSwerveDrivetrain;
-  private final PIDController balanceController;
+    /**
+     * Creates a new AutoBalancing.
+     */
+    private final Drivetrain mSwerveDrivetrain;
+    private final PIDController balanceController;
 
-  private final double BALANCE_GOAL = 0;
+    private final double BALANCE_GOAL = 0;
 
-  private final double BALANCE_kP = 0.05;
-  private final double BALANCE_kI = 0.0;
-  private final double BALANCE_kD = 0.01;
+    private final double BALANCE_kP = 0.05;
+    private final double BALANCE_kI = 0.0;
+    private final double BALANCE_kD = 0.01;
 
-  private final double MAX_OUTPUT = 0.25 * SwerveDrivetrainConstants.MAX_SPEED;
+    private final double MAX_OUTPUT = 0.25 * SwerveDrivetrainConstants.MAX_SPEED;
 
-  private double currentAngle, output; 
+    private double currentAngle, output;
 
-
-  public AutoBalancing(Drivetrain drivetrain) {
-    // Use addRequirements() here to declare subsystem dependencies.
-    mSwerveDrivetrain = drivetrain;
-    addRequirements(mSwerveDrivetrain);
-
-    balanceController = new PIDController(BALANCE_kP, BALANCE_kI, BALANCE_kD);
-    balanceController.setTolerance(1.0);
-  }
+    Timer timer;
+    boolean atSetpoint;
 
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-    balanceController.setSetpoint(0);
-  }
+    public AutoBalancing(Drivetrain drivetrain) {
+        // Use addRequirements() here to declare subsystem dependencies.
+        mSwerveDrivetrain = drivetrain;
+        addRequirements(mSwerveDrivetrain);
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override 
-  public void execute() {
-    currentAngle = mSwerveDrivetrain.getRoll().getDegrees();
+        balanceController = new PIDController(BALANCE_kP, BALANCE_kI, BALANCE_kD);
+        balanceController.setTolerance(1.0);
+    }
 
-    output = MathUtil.clamp(balanceController.calculate(currentAngle, BALANCE_GOAL), -MAX_OUTPUT, MAX_OUTPUT);
 
-    mSwerveDrivetrain.drive(new Translation2d(-output, 0), 0, true, true);
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+        balanceController.setSetpoint(0);
+        atSetpoint = false;
+    }
 
-    
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        currentAngle = mSwerveDrivetrain.getRoll().getDegrees();
 
-    System.out.println("Current Angle " + currentAngle + " Output " + output); 
-    // Logger.getInstance().recordOutput("AutoBalance/Current Angle", currentAngle);
-    // Logger.getInstance().recordOutput("AutoBalance/Current Output Speed", output);
-    
-    
-  }
-  
+        output = MathUtil.clamp(balanceController.calculate(currentAngle, BALANCE_GOAL), -MAX_OUTPUT, MAX_OUTPUT);
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-    mSwerveDrivetrain.stopSwerve();
-  }
+        mSwerveDrivetrain.drive(new Translation2d(-output, 0), 0, true, true);
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return balanceController.atSetpoint();
-  }
+        System.out.println("Current Angle " + currentAngle + " Output " + output);
+
+        if (balanceController.atSetpoint() && !atSetpoint) {
+            timer.start();
+            atSetpoint = true;
+        } else if (!balanceController.atSetpoint() && atSetpoint) {
+            timer.stop();
+            timer.reset();
+            atSetpoint = false;
+        }
+
+    }
+
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        mSwerveDrivetrain.stopSwerve();
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return timer.get() > 1.5;
+    }
 }
